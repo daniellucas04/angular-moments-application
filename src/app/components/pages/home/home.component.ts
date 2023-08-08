b5ef74a44b8c7d520c82ab34dd33f366d43a2b5b
daniellucas04 <daniellsilva@outlook.com>
2023-08-08 16:43:04 -0300

    feat: search in homepage

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -13,15 +13,15 @@ export class HomeComponent implements OnInit{
   allMoments: MomentProps[] = []
   moments: MomentProps[] = []
   baseApiURL = enviroment.baseApiUrl
-
-  //search
+  
+  faSearch = faSearch
+  searchTerm:string = ""
 
   constructor(private momentService: MomentService){}
 
   ngOnInit(): void{
     this.momentService.getMoments().subscribe((items) => {
       const data = items.data
-
       data.map((item) => {
         item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR')
       })
@@ -29,5 +29,12 @@ export class HomeComponent implements OnInit{
       this.allMoments = data
       this.moments = data
     })
+  }
+  
+  search(event: Event): void{
+    const target = event.target as HTMLInputElement
+    const value = target.value
+
+    this.moments = this.allMoments.filter(moment => moment.title.toLocaleLowerCase().includes(value))
   }
 }
